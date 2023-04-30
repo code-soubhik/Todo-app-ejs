f6d8feb8b745a1567c142686f7a33fca8690a9e7
soubhik-111 <soubhikbanerjee04@gmail.com>
2023-04-30 19:30:11 +0530

    env removed

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
+// require('dotenv').config();
 const mongoose = require('mongoose');
 
-const mongoUrl = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_NAME}.mongodb.net/${process.env.COLLECTION_NAME}`;
+// const mongoUrl = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_NAME}.mongodb.net/${process.env.COLLECTION_NAME}`;
+const mongoUrl = `mongodb+srv://mytodo111:No89u9YyH4wikfrx@cluster0.bjtnivp.mongodb.net/Todo-app`;
 
 mongoose.connect(mongoUrl,{useNewUrlParser: true})
 .then(()=>console.log("Connected to the database"))
