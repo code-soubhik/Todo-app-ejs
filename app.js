54d187314887447e263d3bfe233c705196f1a519
soubhik-111 <soubhikbanerjee04@gmail.com>
2023-04-30 18:55:09 +0530

    conn file updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const ejs = require('ejs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const allRoutes = require('./router/allRoutes');
-const conn = require('./db/conn');
+// const conn = require('./db/conn');
 require('./db/conn')
 
 const app = express();
@@ -10,7 +10,7 @@ const port = process.env.PORT || 4444;
 
 app.set('view engine', 'ejs');
 
-app.use(conn)
+// app.use(conn)
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(allRoutes)
--- db/conn.js
+++ db/conn.js
@@ -1,21 +1,8 @@
 require('dotenv').config();
-const express = require('express');
-const conn = new express.Router();
-const mongoUrl = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_NAME}.mongodb.net/${process.env.COLLECTION_NAME}`;
+const mongoose = require('mongoose');
 
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const { default: mongoose } = require('mongoose');
+const mongoUrl = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_NAME}.mongodb.net/${process.env.COLLECTION_NAME}`;
 
 mongoose.connect(mongoUrl,{useNewUrlParser: true})
 .then(()=>console.log("Connected to the database"))
 .catch((error)=>console.log("Error connecting to the database:", error))
-
-conn.use(session({
-    secret: 'foo',
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: mongoUrl})
-}));
-
-module.exports = conn;
