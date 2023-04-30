fda053a472fc1330c8d142cb7ff4d32c2363b474
soubhik-111 <soubhikbanerjee04@gmail.com>
2023-04-30 19:49:30 +0530

    src added

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const path = require('path');
 const ejs = require('ejs');
 const express = require('express');
 const bodyParser = require('body-parser');
-const allRoutes = require('./router/allRoutes');
-require('./db/conn')
+const allRoutes = require('./src/router/allRoutes');
+require('./src/db/conn')
 
 const app = express();
 const port = process.env.PORT || 4444;
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
