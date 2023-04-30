e17701c958b477953c88bcc85ca6c1d8b85248c0
soubhik-111 <soubhikbanerjee04@gmail.com>
2023-04-30 20:52:05 +0530

    string Db_url

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-// const mongoUrl = `mongodb+srv://mytodo111:No89u9YyH4wikfrx@cluster0.bjtnivp.mongodb.net/Todo-app`;
-
 const mongoUrl = process.env.DB_URL
 
 mongoose.connect(mongoUrl,{useNewUrlParser: true})
