6400e977bb115f14bc370247b5adc1e8f13969bc
soubhik-111 <soubhikbanerjee04@gmail.com>
2023-04-30 19:36:43 +0530

    app file path added

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
+const path = require('path');
 const ejs = require('ejs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const allRoutes = require('./router/allRoutes');
-const cors = require('cors');
 require('./db/conn')
 
 const app = express();
 const port = process.env.PORT || 4444;
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(cors())
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(allRoutes)
