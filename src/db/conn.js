026385cfdbfc9e1c746592185e03cf499f496913
soubhik-111 <soubhikbanerjee04@gmail.com>
2023-04-30 21:02:54 +0530

    buffer time

--- src/db/conn.js
+++ src/db/conn.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 
 const mongoUrl = process.env.DB_URL
 
-mongoose.connect(mongoUrl,{useNewUrlParser: true})
+mongoose.connect(mongoUrl,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 30000, // set the timeout to 30 seconds
+})
 .then(()=>console.log("Connected to the database"))
 .catch((error)=>console.log("Error connecting to the database:", error))
