e43740d68bd786a549b5018a5d35ba300af4c227
soubhik-111 <soubhikbanerjee04@gmail.com>
2023-04-30 18:11:28 +0530

    mongo connection

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const ejs = require('ejs');
 const express = require('express');
 const bodyParser = require('body-parser');
-const allRoutes = require('./router/allRoutes');
-const conn = require('./db/conn');
-require('./db/conn')
+const allRoutes = require('./src//router/allRoutes');
+const conn = require('./src/db/conn');
+require('./src/db/conn')
 
 const app = express();
 const port = process.env.PORT || 4444;
